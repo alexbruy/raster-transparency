f52524b132fc635e59857e6f64aed8336ce8fa78
Alexander Bruy <alexander.bruy@gmail.com>
2011-04-07 19:19:39 +0300

    add russian and urrainian language files

---
+++ i18n/rastertransparency_uk_UA.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>QgisApp</name>
+    <message>
+        <location filename="rastertransparency.py" line="118"/>
+        <source>&amp;Raster</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="136"/>
+        <source>Raster calculator ...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RasterTransparency</name>
+    <message>
+        <location filename="rastertransparency.py" line="83"/>
+        <source>Quantum GIS version detected: %1.%2
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="84"/>
+        <source>This version of Raster Transparency requires at least QGIS version 1.5.0
+Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="92"/>
+        <source>Show/hide RasterTransparency dockwidget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="98"/>
+        <source>About RasterTransparency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="207"/>
+        <source>About Raster Transparency</source>
+        <comment>Window title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="209"/>
+        <source>&lt;b&gt;Raster Transparency&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="212"/>
+        <source>Version: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="215"/>
+        <source>Change raster transparency interactively</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="216"/>
+        <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="219"/>
+        <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparency.py" line="236"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RasterTransparencyDockWidget</name>
+    <message>
+        <location filename="rastertransparencydockwidgetbase.ui" line="14"/>
+        <source>Raster Transparency</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparencydockwidgetbase.ui" line="21"/>
+        <source>Values min/max</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rastertransparencydockwidgetbase.ui" line="52"/>
+        <source>Values max/min</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
